4a07f0f2a9c1faf150c1cde195679db4b3821d14
victorSejourne <victor.sejourne@etu.univ-amu.fr>
2024-10-11 00:04:09 +0200

    wip : enemy movements

---
+++ ObsimaPrime/Assets/enemy/FollowPlayer.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject enemy;
+    public float speed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemy.transform.position = Vector3.MoveTowards(enemy.transform.position, player.transform.position, speed);
+    }
+}
